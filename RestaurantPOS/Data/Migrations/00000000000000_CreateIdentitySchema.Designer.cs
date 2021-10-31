72161c33e931e85704c0ef4f8d6cbddb297ad06c
binhbuibksg0123 <74866715+binhbuibksg0123@users.noreply.github.com>
2021-10-31 22:55:41 +0700

    Create Database
    Create Database

--- RestaurantPOS/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ RestaurantPOS/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace RestaurantPOS.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(RestaurantDbContext))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
--- RestaurantPOS/Startup.cs
+++ RestaurantPOS/Startup.cs
@@ -27,11 +27,9 @@ namespace RestaurantPOS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<RestaurantDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }

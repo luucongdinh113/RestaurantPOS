dfb2b249e7eeab5fde8e6f379b12ed14a598501c
binhbuibksg0123 <74866715+binhbuibksg0123@users.noreply.github.com>
2021-11-01 07:13:15 +0700

    Update database
    Update database

--- RestaurantPOS/Migrations/20211101000920_CreateDatabase.Designer.cs
+++ RestaurantPOS/Migrations/20211101000920_CreateDatabase.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantPOS.Data;
 
-namespace RestaurantPOS.Data.Migrations
+namespace RestaurantPOS.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211101000920_CreateDatabase")]
+    partial class CreateDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

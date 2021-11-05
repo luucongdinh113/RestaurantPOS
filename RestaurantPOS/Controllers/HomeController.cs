cce92bedb3a77ec78ee91fa51253c0c740bf9ef5
HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
2021-11-05 11:50:15 +0700

    Sửa tiếng việt

--- RestaurantPOS/Controllers/HomeController.cs
+++ RestaurantPOS/Controllers/HomeController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RestaurantPOS.Models;
 using RestaurantPOS.Services;
-using System.Security.Claims;
-using RestaurantPOS.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace RestaurantPOS.Controllers
 {

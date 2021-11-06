e97af293101e97791367607b406c6cd032687584
luucongdinh113 <83592031+luucongdinh113@users.noreply.github.com>
2021-11-06 15:28:08 +0700

    Origin/feature/order table (#15)
    * Feature Order Table
    
    * update TV of feature orderTable
    
    Co-authored-by: HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>

---
+++ RestaurantPOS/Services/IReservationService.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Services
+{
+    public interface IReservationService
+    {
+        Task<List<BookTableInfoViewModel>> GetAllTableEmptyAsync(DateTime from, DateTime to,int People);
+        Task<bool> BookTableAsync(ClaimsPrincipal user, BookTableInfoViewModel table);
+    }
+}

e6f7746b3f2e915a0573f0420aa74147e5da7ceb
HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
2021-11-07 10:56:03 +0700

    Develop (#22)
    * Create Database
    
    Create Database
    
    * Create feature login/register
    
    Create feature login/register
    
    * Update database
    
    Update database
    
    * update database
    
    update database
    
    * Menu+Food (#5)
    
    * complete feature order-table-history (#6)
    
    * Create payment page (#8)
    
    * Create payment page
    
    * Implement cart
    
    * Sửa tiếng việt
    
    * fix css
    
    * Update CustomerService.cs
    
    * Update ICustomerService.cs
    
    * Update Startup.cs
    
    * Feature/order table history (#9)
    
    * complete feature order-table-history
    
    * re-format code & change output time to localtime & add css for page
    
    * Create feature filter food by choose categories (#11)
    
    * fix html
    
    * Fix Register (#13)
    
    Add email field in register form
    
    * Create reset password function (#12)
    
    * Update site.css
    
    * Origin/feature/order table (#15)
    
    * Feature Order Table
    
    * update TV of feature orderTable
    
    Co-authored-by: HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
    
    * Update _Layout.cshtml
    
    * fix
    
    * Update MenuController.cs
    
    * Save bill not concluding VAT
    
    * Update _Layout.cshtml (#16)
    
    Only vip user can use table reservation
    
    * Origin/feature/order table (#17)
    
    * Feature Order Table
    
    * update TV of feature orderTable
    
    * Update ReservationController.cs
    
    Co-authored-by: HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
    
    * Update _Layout.cshtml
    
    * Feature/filter food (#18)
    
    * Create feature filter food by choose categories
    
    * update style of coding
    
    * Update IFoodService.cs
    
    * Update MenuFood.cshtml
    
    * Update MenuFood.cshtml
    
    * Remove and Sort Usings and Create Payment History (#19)
    
    * Update MenuFood.cshtml (#20)
    
    * Update FoodService.cs
    
    * update payment & payment history
    
    * fix lỗi ngu l :(((
    
    Co-authored-by: binhbuibksg0123 <74866715+binhbuibksg0123@users.noreply.github.com>
    Co-authored-by: Nguyễn Văn Thương <80138135+ThuongNguyenVan158@users.noreply.github.com>
    Co-authored-by: loctran1002 <91211609+loctran1002@users.noreply.github.com>
    Co-authored-by: luucongdinh113 <83592031+luucongdinh113@users.noreply.github.com>
    Co-authored-by: loctran1002 <tienlocak14@gmail.com>
    Co-authored-by: pmk2k1 <91212840+pmk2k1@users.noreply.github.com>

--- RestaurantPOS/Controllers/HomeController.cs
+++ RestaurantPOS/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RestaurantPOS.Models;
 using RestaurantPOS.Services;
@@ -86,95 +86,7 @@ namespace RestaurantPOS.Controllers
         [HttpGet("/ResetPassword")]
         public IActionResult ResetPassword()
         {
-            if (!ModelState.IsValid)
-            {
-                return View(loginViewModel);
-            }
-
-            var loginSucess = await _customerService.LoginAsync(loginViewModel.UserName, loginViewModel.Password);
-
-            if (!loginSucess)
-            {
-                return View(loginViewModel);
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet("/Register")]
-        public IActionResult Register()
-        {
-            return View(new RegisterViewModel());
-        }
-
-        [HttpPost("/Register")]
-        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(registerViewModel);
-            }
-            var registerSucess = await _customerService.RegisterAsync(registerViewModel);
-
-            if (!registerSucess)
-            {
-                return View(registerViewModel);
-            }
-
-            return RedirectToAction("Login");
-        }
-
-        [Route("Logout")]
-        public async Task<IActionResult> Logout()
-        {
-            await _customerService.SignOutAsync();
-            return RedirectToAction("Index");
-        }
-        public async Task<IActionResult> TableOrderedHistory()
-        {
-            if (!User.Identity.IsAuthenticated)
-                return RedirectToAction("Login", "Home");
-            var TbHistory = await _customerService.GetTableHistoryAsync(User);
-            return View(TbHistory);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> ShowToCart()
-        {
-            if (!User.Identity.IsAuthenticated)
-                return RedirectToAction("Login", "Home");
-            var cart = await _customerService.ShowToCartAsync(User);
-            return View(cart);
-        }
-        [HttpPost]
-        public async Task<IActionResult> ShowToCart(CartDetailViewModel cartdetailvm)
-        {
-            if (cartdetailvm.Type == "-")
-            {
-                cartdetailvm.Quantity--;
-            }
-            if (cartdetailvm.Type == "+")
-            {
-                cartdetailvm.Quantity++;
-            }
-            var cart = await _customerService.ShowToCartAsync(User, cartdetailvm);
-            return View(cart);
-        }
-
-        [HttpGet("/Payment")]
-        public async Task<IActionResult> Payment()
-        {
-            if (!User.Identity.IsAuthenticated)
-                return RedirectToAction("Login", "Home");
-            var payment = await _customerService.GetBillToPayAsync(User);
-            return View(payment);
-        }
-
-        [HttpPost("/Payment")]
-        public async Task<IActionResult> Payment(PaymentViewModel billPaymentVM)
-        {
-            await _customerService.UpdatePaymentMethodAsync(User, billPaymentVM);
-            return RedirectToAction("MenuFood", "Menu");
+            return View();
         }
 
         [HttpPost("/ResetPassword")]
@@ -190,7 +102,6 @@ namespace RestaurantPOS.Controllers
             await _customerService.ResetPasswordAsync(resetPassword);
             return RedirectToAction("Login", "Home");
         }
-
         [HttpGet]
         public async Task<IActionResult> ShowToCart()
         {
@@ -244,7 +155,6 @@ namespace RestaurantPOS.Controllers
             var paymentDetail = await _customerService.GetPaymentDetailAsync(id);
             return View(paymentDetail);
         }
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
